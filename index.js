bf0c1c955d9118c66b76a88146b84e5a07ac764a
cagataycali <cagataycali@icloud.com>
2017-09-11 05:12:35 +0300

    ✨  Async-await

--- index.js
+++ index.js
@@ -18,35 +18,37 @@ let list = [
   'fastify'
 ]
 
-const doBench = handler => {
+const fire = () => {
   return new Promise((resolve, reject) => {
-    const spinner = ora(`Started ${handler}`).start()
-    const forked = fork(`${globalModules}/fastify-benchmarks/benchmarks/${handler}`)
-    setTimeout(function () {
-      spinner.color = 'magenta'
-      spinner.text = `Warming ${handler}`
-    }, 500)
     exec(`node ${globalModules}/fastify-benchmarks/node_modules/autocannon -c 100 -d 5 -p 10 localhost:3000`, (err, stdout, stderr) => {
-      if (err) {
-        console.error(`exec error: ${err}`)
-        reject(err)
-      }
-      setTimeout(function () {
-        spinner.color = 'yellow'
-        spinner.text = `Working ${handler}`
-      }, 500)
-      exec(`node ${globalModules}/fastify-benchmarks/node_modules/autocannon -c 100 -d 5 -p 10 localhost:3000`, (err, stdout, stderr) => {
-        if (err) {
-          console.error(`exec error: ${err}`)
-          reject(err)
-        }
-        forked.kill('SIGINT')
-        spinner.text = `Results for ${handler}`
-        spinner.succeed()
-        resolve(stderr)
-      })
+      err ? reject(err) : resolve(stderr)
     })
   })
+}
+
+const doBench = async (handler) => {
+  const spinner = ora(`Started ${handler}`).start()
+  const forked = fork(`${globalModules}/fastify-benchmarks/benchmarks/${handler}`)
+  // Progress status is warming now! #10 -> https://github.com/fastify/fastify-benchmarks/issues/10
+  try {
+    spinner.color = 'magenta'
+    spinner.text = `Warming ${handler}`
+    await fire()
+  } catch (e) {
+    console.log(e)
+  } finally {
+    spinner.color = 'yellow'
+    spinner.text = `Working ${handler}`
+  }
+  try {
+    let result = await fire()
+    forked.kill('SIGINT')
+    spinner.text = `Results for ${handler}`
+    spinner.succeed()
+    return result
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 const start = (list, inc = 0) => {
