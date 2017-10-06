5c95378335a03f19e8fdc188edb82a40c0e57db0
Stefan Aichholzer <play@analogbird.com>
2017-10-06 12:32:56 +1100

    Changes

--- benchmark-bench.js
+++ benchmark-bench.js
@@ -4,67 +4,6 @@
 const inquirer = require('inquirer')
 const bench = require('./lib/bench')
 
-inquirer.prompt([
-  {
-    type: 'confirm',
-    name: 'all',
-    message: 'Do you want to run all benchmark tests?',
-    default: false
-  },
-  {
-    type: 'input',
-    name: 'connection',
-    message: 'How many connection you need?',
-    default: 100,
-    validate (value) {
-      return !Number.isNaN(parseFloat(value)) || 'Please enter a number'
-    },
-    filter: Number
-  },
-  {
-    type: 'input',
-    name: 'pipelining',
-    message: 'How many pipelining you need?',
-    default: 10,
-    validate (value) {
-      return !Number.isNaN(parseFloat(value)) || 'Please enter a number'
-    },
-    filter: Number
-  },
-  {
-    type: 'input',
-    name: 'duration',
-    message: 'How long does it takes?',
-    default: 5,
-    validate (value) {
-      return !Number.isNaN(parseFloat(value)) || 'Please enter a number'
-    },
-    filter: Number
-  }
-]).then((opts) => {
-  if (!opts.all) {
-    select(list => bench(opts, list))
-  } else {
-    bench(opts, [
-      'bare',
-      'connect',
-      'connect-router',
-      'express-route-prefix',
-      'express-with-middlewares',
-      'express',
-      'hapi',
-      'koa-router',
-      'koa',
-      'restify',
-      'take-five',
-      'total.js',
-      'fastify',
-      'micro',
-      'micro-router'
-    ])
-  }
-})
-
 function select (callback) {
   inquirer.prompt([
     {
@@ -129,7 +68,68 @@ function select (callback) {
       }
     }
   ])
-    .then(function (answers) {
-      callback(answers.list)
-    })
+  .then(function (answers) {
+    callback(answers.list)
+  })
 }
+
+inquirer.prompt([
+  {
+    type: 'confirm',
+    name: 'all',
+    message: 'Do you want to run all benchmark tests?',
+    default: false
+  },
+  {
+    type: 'input',
+    name: 'connection',
+    message: 'How many connection you need?',
+    default: 100,
+    validate (value) {
+      return !Number.isNaN(parseFloat(value)) || 'Please enter a number'
+    },
+    filter: Number
+  },
+  {
+    type: 'input',
+    name: 'pipelining',
+    message: 'How many pipelining you need?',
+    default: 10,
+    validate (value) {
+      return !Number.isNaN(parseFloat(value)) || 'Please enter a number'
+    },
+    filter: Number
+  },
+  {
+    type: 'input',
+    name: 'duration',
+    message: 'How long does it takes?',
+    default: 5,
+    validate (value) {
+      return !Number.isNaN(parseFloat(value)) || 'Please enter a number'
+    },
+    filter: Number
+  }
+]).then((opts) => {
+  if (!opts.all) {
+    select(list => bench(opts, list))
+  } else {
+    bench(opts, [
+      'bare',
+      'connect',
+      'connect-router',
+      'express-route-prefix',
+      'express-with-middlewares',
+      'express',
+      'hapi',
+      'koa-router',
+      'koa',
+      'restify',
+      'take-five',
+      'total.js',
+      'fastify',
+      'micro',
+      'micro-router'
+    ])
+  }
+})
