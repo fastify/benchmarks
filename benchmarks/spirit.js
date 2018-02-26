f837599ca0a521bdf1ef2c47bba7c964b0069d66
hnry <hheennrryy@gmail.com>
2018-02-26 13:37:38 -0500

    add spirit to benchmarks
    Related https://github.com/spirit-js/spirit/issues/19

---
+++ benchmarks/spirit.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const http = require('http')
+
+const {adapter, response} = require('spirit').node
+
+const app = (req) => {
+  return response({ hello: 'world' }).type('json')
+}
+
+http.createServer(adapter(app)).listen(3000)
