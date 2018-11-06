30600d5e6889eca8425a1f418f02eaa798aa69c7
Matteo Collina <matteo.collina@gmail.com>
2018-11-06 11:53:14 +0000

    Set full content-type in all frameworks (#76)
    * Set full content-type in all frameworks.
    
    * fix bad copy-paste.

--- benchmarks/connect-router.js
+++ benchmarks/connect-router.js
@@ -5,7 +5,7 @@ const router = require('router')()
 
 const app = connect()
 router.get('/', function (req, res) {
-  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('content-type', 'application/json; charset=utf-8')
   res.end(JSON.stringify({ hello: 'world' }))
 })
 
