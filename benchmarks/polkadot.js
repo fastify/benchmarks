2582f2cd279c3feb846efcfa652850d6bf9f0e83
Luke Edwards <luke.edwards05@gmail.com>
2019-03-26 06:38:16 -0700

    add polkadot

---
+++ benchmarks/polkadot.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const polkadot = require('polkadot');
+
+polkadot(function (req, res) {
+  res.setHeader('content-type', 'application/json; charset=utf-8');
+  res.end(JSON.stringify({ hello: 'world' }));
+}).listen(3000);
