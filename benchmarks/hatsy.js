3da907aae25d3de28ec30ea7021a6c949195dc23
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-06-24 17:36:47 +0700

    Fix linting

--- benchmarks/hatsy.js
+++ benchmarks/hatsy.js
@@ -1,9 +1,9 @@
-const { httpListener } = require('@hatsy/hatsy');
-const { createServer } = require('http');
+const { httpListener } = require('@hatsy/hatsy')
+const { createServer } = require('http')
 
 const server = createServer(httpListener(({ response }) => {
-  response.setHeader('content-type', 'application/json; charset=utf-8');
-  response.end(JSON.stringify({ hello: 'world' }));
-}));
+  response.setHeader('content-type', 'application/json; charset=utf-8')
+  response.end(JSON.stringify({ hello: 'world' }))
+}))
 
-server.listen(3000);
+server.listen(3000)
