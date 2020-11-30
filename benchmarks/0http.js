a61481b8f09243e2b403065b92655475efd0ae46
Matteo Collina <hello@matteocollina.com>
2020-12-01 00:11:17 +0100

    Set content-type for 0http

--- benchmarks/0http.js
+++ benchmarks/0http.js
@@ -4,6 +4,7 @@ const cero = require('0http')
 const { router, server } = cero()
 
 router.get('/', (req, res) => {
+  res.setHeader('content-type', 'application/json; charset=utf-8')
   res.end(JSON.stringify({ hello: 'world' }))
 })
 
