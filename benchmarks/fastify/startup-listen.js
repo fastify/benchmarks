d8ef54ee333806178dec44bd8c8c3c086badb82b
RafaelGSS <rafael.nunu@hotmail.com>
2021-01-26 23:05:48 -0300

    feat: add benchmark by routes variable

--- benchmarks/fastify/startup-listen.js
+++ benchmarks/fastify/startup-listen.js
@@ -1,6 +1,12 @@
 const fastify = require('fastify')
 const server = fastify()
 
+const routes = process.env.routes || 0
+
+for (let i = 0; i < routes; ++i) {
+  server.get(`/${i}`, async () => {})
+}
+
 server.listen(0, () => {
   server.close()
 })
--- benchmarks/fastify/startup.js
+++ benchmarks/fastify/startup.js
@@ -12,8 +12,22 @@ suite.add('raw startup', function (deferred) {
   })
 }, { defer: true })
 
+for (let i = 1; i <= 10_000; i *= 10) {
+  suite.add(`startup with ${i} raw routes`, function (deferred) {
+    new Worker(
+      path.join(__dirname, './startup-listen.js'),
+      {
+        env: {
+          routes: i
+        }
+      }).on('exit', () => {
+        deferred.resolve()
+      })
+  }, { defer: true })
+}
+
 suite.on('cycle', (event) => {
   console.info(String(event.target))
 })
 
-suite.run()
+suite.run({ async: false })
