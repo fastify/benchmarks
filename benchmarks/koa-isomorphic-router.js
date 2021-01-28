e442cf7b10c1d2b3357ca552a08dd6a07a725a0f
Imed Jaberi <imed_jebari@hotmail.fr>
2021-01-28 17:51:22 +0100

    Update koa-isomorphic-router to v1.0.0 (#152)
    * update koa-isomorphic-router deps
    
    * update koa-isomorphic-router benchmark
    
    * update koa-isomorphic-router benchmark --fix-typo

--- benchmarks/koa-isomorphic-router.js
+++ benchmarks/koa-isomorphic-router.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const Koa = require('koa')
-const router = require('koa-isomorphic-router')()
+const Router = require('koa-isomorphic-router')
 
 const app = new Koa()
+const router = new Router()
 
 router.get('/', function (ctx) {
   ctx.body = { hello: 'world' }
