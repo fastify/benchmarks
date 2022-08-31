f7fc58bbed0e90555d296be35af8f7e20b46da0e
Giacomo Rebonato <grebonato@outlook.ie>
2022-08-31 16:02:39 +0100

    descriptive comment

--- benchmarks/trpc-router.js
+++ benchmarks/trpc-router.js
@@ -5,6 +5,7 @@ const { fastifyTRPCPlugin } = require('@trpc/server/adapters/fastify')
 
 const fastify = require('fastify')()
 
+// https://trpc.io/docs/v9/router
 const appRouter = trpc
   .router()
   .query('', {
@@ -18,4 +19,8 @@ fastify.register(fastifyTRPCPlugin, {
   trpcOptions: { router: appRouter, createContext: () => {} }
 })
 
+// Route URL is composed by prefix + query() first string param.
+// In this benchmark, assigning an empty string to both of them is a way for exposing URL "/".
+// A more realistic case would be having prefix="/trpc" and query('tasks'),
+// which would expose the URL "/trpc/tasks"
 fastify.listen({ port: 3000, host: '127.0.0.1' })
