f0fe65d815e18fe009a2c22197d94c6d2148b2c5
Alexander Lichter <github@lichter.io>
2022-10-02 16:02:28 +0200

    chore: update h3

--- benchmarks/h3.js
+++ benchmarks/h3.js
@@ -5,6 +5,6 @@ const { createApp } = require('h3')
 
 const app = createApp()
 
-app.useAsync('/', () => ({ hello: 'world' }))
+app.use('/', () => ({ hello: 'world' }))
 
 createServer(app).listen(process.env.PORT || 3000)
