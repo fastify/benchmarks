f28be6d14fcab1359228a7c6fb1dff6c5f84cf29
Alexander Lichter <github@lichter.io>
2022-10-16 19:45:02 +0200

    fix: use 0.8 methods

--- benchmarks/h3.js
+++ benchmarks/h3.js
@@ -1,10 +1,9 @@
 'use strict'
 
 const { createServer } = require('http')
-const { createApp, eventHandler } = require('h3')
+const { createApp, toNodeListener, eventHandler } = require('h3')
 
 const app = createApp()
+app.use('/', eventHandler(() => ({ hello: 'world' })))
 
-app.use('/', () => eventHandler({ hello: 'world' }))
-
-createServer(app).listen(process.env.PORT || 3000)
+createServer(toNodeListener(app)).listen(process.env.PORT || 3000)
