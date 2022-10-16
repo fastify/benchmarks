4325a17f51f81962abb0bba62194d50669378e1f
Alexander Lichter <github@lichter.io>
2022-10-16 19:41:05 +0200

    chore: upgrade h3

--- benchmarks/h3.js
+++ benchmarks/h3.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const { createServer } = require('http')
-const { createApp } = require('h3')
+const { createApp, eventHandler } = require('h3')
 
 const app = createApp()
 
-app.use('/', () => ({ hello: 'world' }))
+app.use('/', () => eventHandler({ hello: 'world' }))
 
 createServer(app).listen(process.env.PORT || 3000)
