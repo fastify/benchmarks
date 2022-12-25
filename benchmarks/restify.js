dd09efe8c3801beae8a4e4de8d24a137fdc325fe
RafaelGSS <rafael.nunu@hotmail.com>
2022-12-24 21:54:48 -0300

    update: bump all dependencies

--- benchmarks/restify.js
+++ benchmarks/restify.js
@@ -3,8 +3,9 @@
 const restify = require('restify')
 
 const server = restify.createServer()
-server.get('/', function (req, res) {
+server.get('/', function (req, res, next) {
   res.send({ hello: 'world' })
+  return next()
 })
 
-server.listen(3000)
+server.listen(3000, function () {})
