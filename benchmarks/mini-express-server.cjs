d9fd3057432dba2933406c057b2d6a00cd008ea5
Jose Alejandro Concepcion Alvarez <jalejandroc2928@gmail.com>
2023-02-06 20:03:20 +0100

    feat: Fixing the standard problems

--- benchmarks/mini-express-server.cjs
+++ benchmarks/mini-express-server.cjs
@@ -1,8 +1,8 @@
-const { AppServer } = require("mini-express-server");
-const app = new AppServer();
+const { AppServer } = require('mini-express-server')
+const app = new AppServer()
 
-app.get("/", (req, res) => {
-    res.json({ hello: "world" });
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' })
 })
 
-app.listen(3000);
+app.listen(3000)
