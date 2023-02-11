043467488d7f4217a88d2a57dceb4947db889e75
Jose Alejandro Concepcion Alvarez <jalejandroc2928@gmail.com>
2023-02-11 15:12:57 +0100

    feat: Updating the version of the package

--- benchmarks/mini-express-server.cjs
+++ benchmarks/mini-express-server.cjs
@@ -2,7 +2,7 @@ const { AppServer } = require('mini-express-server')
 const app = new AppServer()
 
 app.get('/', (req, res) => {
-  res.json({ hello: 'world' })
+  res.status(200).json({ hello: 'world' })
 })
 
 app.listen(3000)
