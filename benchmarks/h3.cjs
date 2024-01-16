5ca68406ea8f5e73d0d4ad67c793cb7359d8d4f4
Matteo Collina <hello@matteocollina.com>
2024-01-16 13:18:11 +0100

    Force h3 to send the correct content-type for JSON

--- benchmarks/h3.cjs
+++ benchmarks/h3.cjs
@@ -1,9 +1,14 @@
 'use strict'
 
 const { createServer } = require('node:http')
-const { createApp, toNodeListener, eventHandler } = require('h3')
+const { createApp, toNodeListener, eventHandler, setHeader } = require('h3')
 
 const app = createApp()
-app.use('/', eventHandler(() => ({ hello: 'world' })))
+app.use('/', eventHandler((ev) => {
+  // Unfortunatly, we need to set the content-type manually
+  // to level the paying field
+  setHeader(ev, 'content-type', 'application/json; charset=utf-8')
+  return { hello: 'world' }
+}))
 
 createServer(toNodeListener(app)).listen(process.env.PORT || 3000)
