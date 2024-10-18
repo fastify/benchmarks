51ced54bdc5c3d25cb1a1e30620bf8d6044b070e
francesco <francesco.bagnoli.69@gmail.com>
2024-10-18 12:23:32 +0200

    Create ultimate-express.cjs
    Signed-off-by: francesco <francesco.bagnoli.69@gmail.com>

---
+++ benchmarks/ultimate-express.cjs
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('ultimate-express')
+
+const app = express()
+
+app.disable('etag')
+app.disable('x-powered-by')
+
+app.get('/', function (req, res) {
+  res.json({ hello: 'world' })
+})
+
+app.listen(3000)
