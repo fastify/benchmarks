12b3a07f9c8c02cf238c53fe647061c6d7d98999
Romain Lanz <2793951+RomainLanz@users.noreply.github.com>
2024-10-18 19:55:23 +0200

    Add AdonisJS (#343)

---
+++ benchmarks/adonisjs.mjs
@@ -0,0 +1,31 @@
+import { createServer } from 'node:http'
+import { defineConfig, Server } from '@adonisjs/http-server'
+import { Logger } from '@adonisjs/logger'
+import { Emitter } from '@adonisjs/events'
+import { Encryption } from '@adonisjs/encryption'
+import { Application } from '@adonisjs/application'
+
+const app = new Application(new URL('./', import.meta.url), {
+  environment: 'web',
+  importer: () => {}
+})
+
+await app.init()
+
+const encryption = new Encryption({ secret: 'averylongrandom32charslongsecret' })
+
+const server = new Server(
+  app,
+  encryption,
+  new Emitter(app),
+  new Logger({ enabled: false }),
+  defineConfig({})
+)
+
+server.getRouter().get('/', async (ctx) => {
+  return ctx.response.send({ hello: 'world' })
+})
+
+await server.boot()
+
+createServer(server.handle.bind(server)).listen(3000)
