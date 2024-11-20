a45b6d153dd34474b184b0c02894eae5ed0eeb75
Frazer Smith <frazer.dev@icloud.com>
2024-11-20 14:23:03 +0000

    fix(koa-router): use new package and router inst

--- benchmarks/koa-router.cjs
+++ benchmarks/koa-router.cjs
@@ -1,9 +1,10 @@
 'use strict'
 
 const Koa = require('koa')
-const router = require('koa-router')()
+const Router = require('@koa/router')
 
 const app = new Koa()
+const router = new Router()
 
 router.get('/', async function (ctx) {
   ctx.body = { hello: 'world' }
