b2c116a54e910a029118c4f9b4d2739abce38a24
Szymon Łągiewka <slagiewka@users.noreply.github.com>
2025-09-13 16:27:18 +0200

    chore: retire pacakges not meeting requirements
    All of them are currently below 500 weekly downloads and no updates for
    2-8 years [^1].
    
    `micro-route` has a recent (couple of months) activity above 500/week,
    but the repository is inactive (no issues or PRs).
    
    [^1]:https://npmtrends.com/@leizm/web-vs-micro-route-vs-polkadot-vs-rayo-vs-server-base-vs-server-base-router-vs-take-five

--- benchmarks/polkadot.cjs
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const polkadot = require('polkadot')
-
-polkadot(function (_req, res) {
-  res.setHeader('content-type', 'application/json; charset=utf-8')
-  res.end(JSON.stringify({ hello: 'world' }))
-}).listen(3000)
